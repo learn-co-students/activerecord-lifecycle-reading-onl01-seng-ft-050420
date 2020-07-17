0f4e523aaaf16f4788dd392494eb65437066584b
codewithirene567 <irenejinscott@gmail.com>
2020-07-17 11:59:36 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,10 @@ class Post < ActiveRecord::Base
 
   belongs_to :author
   validate :is_title_case 
+#write before after the belongs to and validates but before other methods
+  before_validation :make_title_case
 
+  before_save :email_author_about_post
   private
 
   def is_title_case
@@ -11,7 +14,13 @@ class Post < ActiveRecord::Base
     end
   end
 
-  def make_title_case
+  def email_author_about_post
+    # Not implemented.
+    # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
+  end
+  
+  def make_title_case # in order to make sure
+    # the validation passes before every save we create this method
     self.title = self.title.titlecase
   end
 end
